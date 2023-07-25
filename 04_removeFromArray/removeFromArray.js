f6bc8bc1916f3ab612ef4103d3fe397d75f588a3
1s-dev <137394627+1s-dev@users.noreply.github.com>
2023-07-25 19:34:54 +0700

    Fixed 04 removeFromArray and pass all of its tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  let index = "";
+  let finalArray = "";
+  console.log(...args);
+  console.log(args.length);
+  console.log(array.indexOf(args[0]));
+  for (let i = 0; i < args.length; i++) {
+    index = array.indexOf(args[i]);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
+
+/*
+const removeFromArray = function (array, input) {
+  const index = array.indexOf(input);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+  return array;
+};
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
