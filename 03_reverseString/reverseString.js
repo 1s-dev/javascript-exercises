5001f4b27ef3e99268743c50919e06e6e2b22c0e
1s-dev <137394627+1s-dev@users.noreply.github.com>
2023-07-25 17:21:13 +0700

    Fixed 03 reverseString and run all of its tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let splittedString = input.split("");
+  let reversedString = splittedString.reverse();
+  console.log(reversedString);
+  let combinedString = reversedString.join("");
+  return combinedString;
 };
 
 // Do not edit below this line
