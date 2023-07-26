d5c395ce532cc34dee5fe944769fd858b0efea10
1s-dev <137394627+1s-dev@users.noreply.github.com>
2023-07-26 22:04:36 +0700

    Fixed 07 tempCoversion and pass all of its tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,33 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let rawCelsius = (temp - 32) * (5 / 9);
+  let roundedCelsius = parseFloat(rawCelsius.toFixed(1));
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let rawFahrenheit = (temp * 9) / 5 + 32;
+  let roundedFahrenheit = parseFloat(rawFahrenheit.toFixed(1));
+  return roundedFahrenheit;
 };
+
+/*
+Celsius = (Fahrenheit - 32) * (5 / 9)
+function convertToCelsius (temp) {
+  let rawCelsius = (temp - 32) * (5 / 9);
+  let roundedCelsius = parseFloat(rawCelsius.toFixed(1))
+  return roundedCelsius
+}
+
+Fahrenheit = (Celsius * 2) + 30
+function convertToFahrenheit (temp) {
+  let rawFahrenheit = (temp * 2) + 30;
+  let roundedFahrenheit = parseFloat(rawFahrenheit.toFixed(1))
+  return roundedFahrenheit
+}
+*/
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
