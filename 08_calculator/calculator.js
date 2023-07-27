66a534e5fff7126fd1dc3fd26ebff28543b78107
1s-dev <137394627+1s-dev@users.noreply.github.com>
2023-07-27 21:48:30 +0700

    Fixed 08 calculator and pass all of its tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arraySum) {
+  let total = 0;
+  for (let i = 0; i <= arraySum.length - 1; i++) {
+    total += arraySum[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arrayMultiply) {
+  let total = 1;
+  if (arrayMultiply.length < 1) {
+    total = 0;
+    return total;
+  } else {
+    for (let i = 0; i <= arrayMultiply.length - 1; i++) {
+      total *= arrayMultiply[i];
+    }
+    return total;
+  }
 };
 
-const power = function() {
-	
+const power = function (num, raisedPower) {
+  return Math.pow(num, raisedPower);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  if (num < 1) {
+    return total;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      total *= i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
