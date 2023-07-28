2e7f52fd803353070559133548657400e0d2e248
1s-dev <137394627+1s-dev@users.noreply.github.com>
2023-07-28 22:51:07 +0700

    Fixed part of 12 findTheOldest and pass the first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let age = "";
+  const array = [];
+  for (let i = 0; i < people.length; i++) {
+    age = people[i].yearOfDeath - people[i].yearOfBirth;
+    const obj = { name: people[i].name, age: age };
+    array.push(obj);
+  }
+  array.sort(function (a, b) {
+    return b.age - a.age;
+  });
+  console.log(array[0]);
+  return array[0];
 };
+
+/*
+
+
+let age = "";
+const array = [];
+for (let i = 0; i < arrayObj.length; i++) {
+  age = yearOfDeath - yearOfBirth;
+  const obj = {name: arrayObj[i], age: age}
+  array.push(obj);
+}
+array.sort(function(a, b){return b.age - a.age});
+return array[0].name;
+
+*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
