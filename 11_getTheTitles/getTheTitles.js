854477200d63d180305ede5eef4decf5f6fbd195
1s-dev <137394627+1s-dev@users.noreply.github.com>
2023-07-28 20:44:23 +0700

    Fixed getTheTitles and pass its test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arrayObj) {
+  array = [];
+  for (let i = 0; i < arrayObj.length; i++) {
+    array.push(arrayObj[i].title);
+  }
+  return array;
 };
 
 // Do not edit below this line
